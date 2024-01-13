df5cea336f2f3498e1bf33f91a77ab34f5debe2a
raja <rajasimhabolla@gmail.com>
2024-01-13 14:34:55 +0530

    Fix compose_email function and add fetch request for sending emails

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -21,26 +21,31 @@ function compose_email() {
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
   document.querySelector('#submit').addEventListener('click', (event) => {
-    const to = document.querySelector("#compose-recipients").value;
+    let to = document.querySelector("#compose-recipients").value;
     let fr = document.querySelector(".form-control").value;
     let subject = document.querySelector("#compose-subject").value;
     let content = document.querySelector("#compose-body").value; // Changed "Body" to "#compose-body"
     alert(`To: ${to}, From: ${fr}, Subject: ${subject}, Content: ${content}`); // Concatenated the values into a single string
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: to,
+          subject:subject,
+          body: content
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        if (result.status == 201){
+          console.log(result["message"])
+        }
+        else{
+          alert(result["error"]);
+        }
+    });
     load_mailbox('mail');
   });
-  fetch('/mails',{
-    'method':'POST',
-    body:JSON.stringify({
-
-      subject:subject,
-      body:content
-    })
-    .then(()=>response.json())
-    .then(result=>{
-      console.log(result)
-    })
-
-  })
 }
 
 function load_mailbox(mailbox) {
@@ -50,4 +55,10 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  let url = "/emails/"+mailbox
+  fetch(url)
+  .then(response=>response.json())
+  .then(result=>{
+    
+  });
 }
